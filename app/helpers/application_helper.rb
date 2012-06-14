b6d3feb78ed895e5ac69872e9d7bd025c27bb3b5
theofanislekkas <theofanis.lekkas@theofanislekkas.com>
2012-06-14 18:57:17 -0500

    Add title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
-module ApplicationHelper
+module ApplicationHelper #This is a constant, we can tell contstants in
+	#Ruby b/c it starts w/ a capital letter.
+
+	#Returns the full title on a per-page basis.
+	def full_title(page_title)
+		base_title = 'Ruby on Rails Tutorial Sample App'
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
