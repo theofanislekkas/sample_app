b6fba55a3d0c59771679ffd1e7e5b72909aa1359
theofanislekkas <theofanis.lekkas@theofanislekkas.com>
2012-06-19 17:55:29 -0500

    Change to Ruby 1.9-style hash syntax.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,6 +18,7 @@ describe "StaticPages" do
     it "should not have a custom page title" do
     	visit '/static_pages/home'
     	page.should_not have_selector('h1', :text => '| Home')
+    end
   end  
 
   describe "Help page" do
